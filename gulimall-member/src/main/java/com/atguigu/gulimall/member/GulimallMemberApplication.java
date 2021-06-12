73484cb5a7f9e12dd6149a2c6fcb41c25f02bfd4
董霁辉 <57429361+dongjihui666@users.noreply.github.com>
2021-06-12 13:59:16 +0900

    代码生成器,生成后台代码

--- gulimall-member/src/main/java/com/atguigu/gulimall/member/GulimallMemberApplication.java
+++ gulimall-member/src/main/java/com/atguigu/gulimall/member/GulimallMemberApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GulimallMemberApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(GulimallMemberApplication.class, args);
+    public static void main(String[] args){
+        SpringApplication.run(GulimallMemberApplication.class,args);
     }
 
 }
--- gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
@@ -1,13 +1,23 @@
 package com.atguigu.gulimall.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 1整合Mybatis-plus
+ * 1),导入依赖
+ * 2)配置
+ *   1 配置数据源
+ *     1)导入数据库的驱动.
+ *   2 配置mybatis-plus相关的详细
+ *     1)使用@mapperScan
+ *     2
+ */
+@MapperScan(basePackages = "com.atguigu.gulimall.product.dao")
 @SpringBootApplication
 public class GulimallProductApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(GulimallProductApplication.class, args);
+    public static void main(String[] args){
+        SpringApplication.run(GulimallProductApplication.class,args);
     }
-
 }
