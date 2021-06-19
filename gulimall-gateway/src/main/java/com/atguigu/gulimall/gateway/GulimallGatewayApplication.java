7f36ab1eb0f08eeb197930d9ef873275f8e6b432
董霁辉 <57429361+dongjihui666@users.noreply.github.com>
2021-06-19 20:40:08 +0900

    配置gateway

---
+++ gulimall-gateway/src/main/java/com/atguigu/gulimall/gateway/GulimallGatewayApplication.java
@@ -0,0 +1,24 @@
+package com.atguigu.gulimall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 1 开启服务注册发现
+ *  (配置nacos的注册中心地址)
+ *
+ *  排除跟数据源有关的配置
+ *  exclude = {DataSourceAutoConfiguration.class
+ *
+ */
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class GulimallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallGatewayApplication.class, args);
+    }
+
+}
